6ca1f68a39003d4e993c244abca28022f07ea35e
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-08-14 00:12:23 -0400

    Update main.go

--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
 
 func main() {
-  fmt.Println("Hello, World!")
+	http.Handle("/", http.HandlerFunc(handleRequest))
+	http.ListenAndServe(":8080", nil)
+}
+
+func handleRequest(httpWriter http.ResponseWriter, r *http.Request) {
+	httpWriter.WriteHeader(http.StatusNotFound)
+	httpWriter.Header().Set("Content-Type", "application/json")
+	type errorMessage struct {
+		Message string
+	}
+	errorMsg := errorMessage{
+		Message: "Resource Not Found",
+	}
+	errorJsonMessage, err := json.Marshal(errorMsg)
+	if err != nil {
+		log.Println("error:", err)
+	}
+	httpWriter.Write(errorJsonMessage)
 }
