de65480879f5af9a0e4357b2b60ecf96b06d6647
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-08-14 11:16:44 -0400

    Update main.go

--- main.go
+++ main.go
@@ -12,17 +12,33 @@ func main() {
 }
 
 func handleRequest(httpWriter http.ResponseWriter, r *http.Request) {
+	// Set the header to status not found.
 	httpWriter.WriteHeader(http.StatusNotFound)
+	// Set the content type to application/json.
 	httpWriter.Header().Set("Content-Type", "application/json")
+	// Set the body to an error message.
 	type errorMessage struct {
+		Code    int
 		Message string
 	}
 	errorMsg := errorMessage{
-		Message: "Resource Not Found",
+		Code:    http.StatusNotFound,
+		Message: "Resource not found",
 	}
-	errorJsonMessage, err := json.Marshal(errorMsg)
+	// Wrap the error in a error object.
+	type jsonError struct {
+		Error errorMessage
+	}
+	// The content of the error object.
+	jsonReturn := jsonError{
+		Error: errorMsg,
+	}
+	// Marshal the error message to JSON.
+	errorJsonMessage, err := json.Marshal(jsonReturn)
+	// Log the error if there is one.
 	if err != nil {
-		log.Println("error:", err)
+		log.Println(err)
 	}
+	// Write the JSON error message.
 	httpWriter.Write(errorJsonMessage)
 }
